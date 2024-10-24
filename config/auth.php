2f697e04f57f1f5a441591b540f2623fb65ed851
s_abdulldyev <S.Abdulldyev@uzinfocom.uz>
2024-10-24 14:15:11 +0500

    Fixing typo from doamin customers for User model

--- config/auth.php
+++ config/auth.php
@@ -61,8 +61,8 @@ return [
 
     'providers' => [
         'users' => [
-            'driver' => 'eloquent',
-            'model' => env('AUTH_MODEL', App\Models\User::class),
+                'driver' => 'eloquent',
+                'model' => env('AUTH_MODEL', Domains\Customer\Models\User::class),
         ],
 
         // 'users' => [
