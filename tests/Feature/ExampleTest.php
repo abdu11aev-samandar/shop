6fa78e7d73378f4ad0f4eb8b7bcde1cd9e8543aa
s_abdulldyev <S.Abdulldyev@uzinfocom.uz>
2024-10-28 11:10:09 +0500

    Converting to pest

--- tests/Feature/ExampleTest.php
+++ tests/Feature/ExampleTest.php
@@ -1,19 +1,8 @@
 <?php
 
-namespace Tests\Feature;
+use function Pest\Laravel\get;
 
-// use Illuminate\Foundation\Testing\RefreshDatabase;
-use Tests\TestCase;
-
-class ExampleTest extends TestCase
-{
-    /**
-     * A basic test example.
-     */
-    public function test_the_application_returns_a_successful_response(): void
-    {
-        $response = $this->get('/');
-
-        $response->assertStatus(200);
-    }
-}
+it('receives a HTTP OK on the home page', function () {
+    get(route('home'))
+        ->assertStatus(\JustSteveKing\StatusCode\Http::OK);
+});
